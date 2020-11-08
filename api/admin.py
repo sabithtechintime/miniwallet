302b6666db95e7e1abd00a2ed30e0a0b44ace170
SABITH <SADITH@SABITHs-MacBook-Pro.local>
2020-11-08 19:32:19 +0530

    provision to view ID from backend

--- api/admin.py
+++ api/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from api.models import Wallet, Transaction, CustomUser
 
 # Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username')
+
 
 admin.site.register(Wallet)
 admin.site.register(Transaction)
-admin.site.register(CustomUser)
+# admin.site.register(CustomUser)
